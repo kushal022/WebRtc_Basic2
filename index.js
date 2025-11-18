69baca54f1a6b70f54aac5e7fe5c3343a4215524
kushal022 <kushla.learn22@gmail.com>
2025-11-18 14:40:36 +0530

    update

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 const { Server } = require("socket.io");
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
 
-const io = new Server(8000, {
-    cors: {
-    origin: ["http://localhost:5173", "https://phenomenal-meringue-084d5c.netlify.app/"],
+const app = express();
+const server = http.createServer(app);
+
+app.use(
+  cors({
+    origin: [
+      "https://phenomenal-meringue-084d5c.netlify.app",
+      "http://localhost:5173"
+    ],
     methods: ["GET", "POST"],
-    credentials: true
-  }
+    credentials: true,
+  })
+);
+
+const io = new Server(server, {
+  cors: {
+    origin: [
+      "https://phenomenal-meringue-084d5c.netlify.app",
+      "http://localhost:5173"
+    ],
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
 });
+
+// const { Server } = require("socket.io");
+
+// const io = new Server(8000, {
+//     cors: {
+//     origin: ["http://localhost:5173", "https://phenomenal-meringue-084d5c.netlify.app/"],
+//     methods: ["GET", "POST"],
+//     credentials: true
+//   }
+// });
 
 const emailToSocketIdMap = new Map();
 const socketIdToEmailMap = new Map();
@@ -44,4 +74,10 @@ io.on("connection", (socket)=> {
     socket.on('peer:nego:done', ({ to, ans })=>{
         io.to(to).emit("peer:nego:final", { from: socket.id, ans});
     })
+});
+
+
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
