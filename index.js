712feea2fe1f8fd76247f55323138a055ff644ca
kushal022 <kushla.learn22@gmail.com>
2025-11-18 12:59:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const { Server } = require("socket.io");
+
+const io = new Server(8000, {cors: true});
+
+const emailToSocketIdMap = new Map();
+const socketIdToEmailMap = new Map();
+
+io.on("connection", (socket)=> {
+    console.log('Socket Connected: ' , socket.id);
+
+    socket.on('room:join', (data) => {
+        const { email, room } = data;
+        emailToSocketIdMap.set(email, socket.id);
+        socketIdToEmailMap.set(socket.id, email);
+        // Notify all Room members:
+        io.to(room).emit("user:joined", { email, id: socket.id })
+        // Join Room:
+        socket.join(room);
+        // Notify self joined the room
+        io.to(socket.id).emit("you-joined-room", data );
+    });
+
+    //Make call- Incomming call
+    socket.on("user:call", ({to, offer}) => {
+        console.log("in make call and emit incomming call")
+        io.to(to).emit("incomming:call", { from: socket.id, offer })
+    })
+
+    socket.on("call:accepted", ({ to, ans }) => {
+        console.log("In call accept ")
+        io.to(to).emit("call:accepted", { from: socket.id, ans })
+    })
+
+    socket.on("peer:nego:needed", ({ to, offer })=>{
+        io.to(to).emit("peer:nego:needed", { from: socket.id, offer })
+    })
+
+    socket.on('peer:nego:done', ({ to, ans })=>{
+        io.to(to).emit("peer:nego:final", { from: socket.id, ans});
+    })
+});
